7c5b395cc76f5143f928db93136ce7c35ec6fd0a
akashkanade9182 <akashkanade235@gmail.com>
2022-12-14 13:33:34 +0530

    setup

---
+++ Gadgetbest.com/frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./Components/Navbar.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    
+    </div>
+  );
+}
+
+export default App;
