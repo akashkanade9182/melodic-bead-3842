4322ae736c3f0b75b12d0fc5fc4508e3639ae39d
Arun-Jawlia <arunkumar08.mk@gmail.com>
2022-12-15 19:12:01 +0530

    day-3

---
+++ Gadgetbest.com/frontend/src/Components/AlertCart.jsx
@@ -0,0 +1,16 @@
+import { Alert, AlertIcon } from '@chakra-ui/react'
+import React from 'react'
+
+const AlertCart = () => {
+
+
+
+  return (
+    <Alert status="success">
+      <AlertIcon />
+      Data uploaded to the server. Fire on!
+    </Alert>
+  )
+}
+
+export default AlertCart
