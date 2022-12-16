8822606e21ed96ccbfd9d669a251aae52fc87ec7
Arun-Jawlia <arunkumar08.mk@gmail.com>
2022-12-16 12:03:00 +0530

    a-3

--- Gadgetbest.com/frontend/src/App.js
+++ Gadgetbest.com/frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar.jsx"
 import Productspage from './Pages/Productspage';
+import SingleProduct from './Pages/SingleProduct';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
-      <Productspage/>
+      {/* <Productspage/> */}
+      <SingleProduct/>
     
     </div>
   );
