4a16518725ba46c910490140c217d9292a484164
iamamitkumarsaini <iamaksaini68@gmail.com>
2022-12-20 12:50:43 +0530

    day6

--- Gadgetbest.com/frontend/src/App.js
+++ Gadgetbest.com/frontend/src/App.js
@@ -1,21 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from "./Components/Navbar.jsx"
-
-import Productspage from './Pages/Productspage';
-import SingleProduct from './Pages/SingleProduct';
-
-import { AllRoutes } from './Pages/AllRoutes';
-import CheckoutPage from './Pages/CheckoutPage';
-import PaymentPage from './Pages/PaymentPage';
-
-
-// import Productspage from './Pages/Productspage';
-
+import AllRoutes from './Pages/AllRoutes';
 
 function App() {
   return (
     <div className="App">
+      <AllRoutes />
     </div>
   );
 }
--- Gadgetbest.com/frontend/src/Pages/AllRoutes.jsx
+++ Gadgetbest.com/frontend/src/Pages/AllRoutes.jsx
@@ -14,3 +14,5 @@ export const AllRoutes = () => {
     </div>
   );
 };
+
+export default AllRoutes;
